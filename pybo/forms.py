bcedcab084ab10ee09a2a20a02754366da9c3b8b
iensud184-max <iensud184@gmail.com>
2026-04-14 13:09:31 +0900

    20260414_signup_zzinzzin

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms.fields.simple import StringField, TextAreaField, PasswordField, EmailField
+from wtforms.fields.simple import StringField, TextAreaField, PasswordField, EmailField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class UserCreateForm(FlaskForm):
@@ -9,6 +9,9 @@ class UserCreateForm(FlaskForm):
     password1 = PasswordField('비밀번호', validators=[DataRequired('필수 입력 항목입니다.'), EqualTo('password2', message='비밀번호가 일치하지 않습니다.')])
     password2 = PasswordField('비밀번호 확인', validators=[DataRequired('필수 입력 항목입니다.')])
     email = EmailField('이메일', validators=[DataRequired('필수 입력 항목입니다.'), Email()])
+    Terms_of_Service = BooleanField('회원이용약관 동의 (필수)', validators=[DataRequired('필수 사항입니다.')])
+    Privacy_Policy = BooleanField('개인정보처리방침 동의 (필수)', validators=[DataRequired('필수 사항입니다.')])
+    receive_emails = BooleanField('이메일 수신 동의 (선택)')
 
 class UserLoginForm(FlaskForm):
     userid = StringField('아이디', validators=[DataRequired('필수 입력 항목입니다.'), Length(min=3, max=25)])
