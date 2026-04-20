9cafb5b33a9eced47c5fcbac460a74e704f33fd7
bomi6231835-jpg <bomi6231835@gmail.com>
2026-04-20 13:19:29 +0900

    20260420-review_detail

--- pybo/models.py
+++ pybo/models.py
@@ -96,11 +96,12 @@ class Reservation(db.Model):
     seat_id = db.Column(db.Integer)
 
 # 1대1 문의 - review __공자사항
-class Privacy(db.Model):
+class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(100), nullable=False)
     info = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
+    image_path = db.Column(db.Text(), nullable=True)
     create_date = db.Column(db.DateTime(), nullable=False)
     # user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     # user = db.relationship('User', backref=db.backref('answer_set'))
--- pybo/views/cs_views.py
+++ pybo/views/cs_views.py
@@ -4,11 +4,12 @@ from idlelib import query
 
 from flask import Blueprint, render_template, request, redirect, url_for
 
-from pybo.models import Privacy
+
 from pybo.models import Faq
 from pybo import db
-from pybo.forms import NoticeForm, AnswerForm
-from pybo.models import Notice, Answer
+from pybo.forms import NoticeForm, AnswerForm, ReviewForm
+from pybo.models import Notice
+from pybo.models import Review
 
 
 bp = Blueprint('cs',__name__, url_prefix='/cs')
@@ -32,7 +33,7 @@ def notice_detail(notice_id):
     next_notice = Notice.query.get(notice_id + 1)
     return render_template("cs/notice/notice_detail.html", notice=notice_detail, prev_notice=prev_notice, next_notice=next_notice)
 
-@bp.route("/faq/list")
+@bp.route("/faq/")
 def faq_list():
     page = request.args.get('page', type=int, default=1)
     faq_list = Faq.query.order_by(Faq.create_date.desc())
@@ -40,10 +41,16 @@ def faq_list():
     print(faq_list)
     return render_template("cs/faq/faq.html", faq_list=faq_list)
 
-@bp.route("/privacy/")
-def privacy_list():
-    # page = request.args.get('page', type=int, default=1)
-    privacy_list = Privacy.query.all()
-    return render_template("cs/review/review.html", privacy_list=privacy_list)
+@bp.route("/review/")
+def review_list():
+    page = request.args.get('page', type=int, default=1)
+    review_list = Review.query.all()
+    return render_template("cs/review/review.html", review_list=review_list)
+
+@bp.route('/create/')
+def create():
+    form = ReviewForm()
+    return render_template('review/review_form.html', form=form)
+
 
 
